2b0dd740ebf8cc5354a048909cfb78fcfc9606f1
AkashArul26 <95226047+AkashArul26@users.noreply.github.com>
2024-01-18 13:58:48 +0530

    Committed the sample

---
+++ WTP/Program.cs
@@ -0,0 +1,27 @@
+﻿using Syncfusion.DocIO.DLS;
+using Syncfusion.DocIORenderer;
+using Syncfusion.Pdf;
+
+using (FileStream docStream = new FileStream("../../../Template.docx", FileMode.Open, FileAccess.Read))
+{
+    // Loads file stream into Word document
+    using (WordDocument wordDocument = new WordDocument(docStream, Syncfusion.DocIO.FormatType.Automatic))
+    {
+        // Instantiation of DocIORenderer for Word to PDF conversion
+        using (DocIORenderer render = new DocIORenderer())
+        {
+            // Sets true to embed complete TrueType fonts
+            render.Settings.EmbedCompleteFonts = true;
+
+            // Converts Word document into PDF document
+            PdfDocument pdfDocument = render.ConvertToPDF(wordDocument);
+            // Saves the PDF file
+            using (FileStream outputStream = new FileStream(@"Output.pdf", FileMode.OpenOrCreate))
+            {
+                pdfDocument.Save(outputStream);
+                // Closes the instance of PDF document object
+                pdfDocument.Close();
+            }
+        }
+    }
+}
